09ad2e413c3c785ac2b583e84ededacbb17962d0
johnmotta <gabriel_motta31@hotmail.com>
2023-08-27 15:39:31 -0300

    Translate test comments to English

--- SnakeGameTests/SnakeGameTests.swift
+++ SnakeGameTests/SnakeGameTests.swift
@@ -7,9 +7,9 @@ class SnakeGameTests: XCTestCase {
 
     func testInitialState() {
         let gameState = GameState()
-        // Testa se a cobra começa com tamanho 0
+        // Tests if the snake starts with size 0
         XCTAssertEqual(gameState.snake.body.count, 0)
-        // Testa se a comida é gerada em uma posição válida
+        // Tests if the food is generated in a valid position
         XCTAssertTrue(gameState.food.position.x >= 0 && gameState.food.position.x < GameState.gridWidth)
         XCTAssertTrue(gameState.food.position.y >= 0 && gameState.food.position.y < GameState.gridHeight)
     }
@@ -21,15 +21,14 @@ class SnakeGameTests: XCTestCase {
         gameState.updateDirection(.right)
         gameState.tick()
 
-        // Testa se a cobra se moveu para a direita
+        // Tests if the snake moved to the right
         XCTAssertEqual(gameState.snake.head, Position(x: initialPosition.x + 1, y: initialPosition.y))
     }
-
 
     func testSnakeGrow() {
         let gameState = GameState()
 
-        // Coloca a comida diretamente na frente da cobra
+        // Places the food directly in front of the snake
         let initialPosition = gameState.snake.head
         gameState.updateFoodPosition(Position(x: initialPosition.x + 1, y: initialPosition.y))
 
@@ -37,13 +36,14 @@ class SnakeGameTests: XCTestCase {
         gameState.updateDirection(.right)
         gameState.tick()
 
+        // Tests if the snake grew
         XCTAssertEqual(gameState.snake.body.count, initialLength + 1)
     }
     
     func testCollisionWithBorder() {
         let gameState = GameState()
 
-        // Move a cobra até a borda
+        // Moves the snake to the edge
         for _ in 0..<GameState.gridWidth - 1 {
             gameState.updateDirection(.right)
             gameState.tick()
@@ -51,11 +51,11 @@ class SnakeGameTests: XCTestCase {
 
         let initialLength = gameState.snake.body.count
 
-        // Tenta mover a cobra além da borda
+        // Tries to move the snake beyond the edge
         gameState.updateDirection(.right)
         gameState.tick()
 
-        // Verifica se a cobra foi reiniciada
+        // Checks if the snake was reset
         XCTAssertEqual(gameState.snake.head, Position(x: 5, y: 5))
         XCTAssertEqual(gameState.snake.body.count, initialLength)
     }
@@ -63,7 +63,7 @@ class SnakeGameTests: XCTestCase {
     func testCollisionWithBorders() {
         let gameState = GameState()
 
-        // Mova a cobra para a borda direita
+        // Moves the snake to the right edge
         while gameState.snake.head.x < GameState.gridWidth - 1 {
             gameState.updateDirection(.right)
             gameState.tick()
@@ -71,12 +71,12 @@ class SnakeGameTests: XCTestCase {
 
         let initialHeadPosition = gameState.snake.head
 
-        // Mova a cobra mais um passo para a direita para forçar a colisão com a borda
+        // Moves the snake one more step to the right to force a collision with the edge
         gameState.updateDirection(.right)
         gameState.tick()
 
-        // Verifica se a cobra colidiu com a borda e foi reiniciada
-        XCTAssertNotEqual(gameState.snake.head, initialHeadPosition, "A cobra não foi reiniciada após colidir com a borda")
-        XCTAssertEqual(gameState.snake.body.count, 0, "A cobra deve ter o tamanho inicial após reiniciar")
+        // Checks if the snake collided with the edge and was reset
+        XCTAssertNotEqual(gameState.snake.head, initialHeadPosition, "The snake was not reset after colliding with the edge")
+        XCTAssertEqual(gameState.snake.body.count, 0, "The snake should have the initial size after resetting")
     }
 }
--- SnakeGameUITests/SnakeGameUITests.swift
+++ SnakeGameUITests/SnakeGameUITests.swift
@@ -6,11 +6,11 @@ final class SnakeGameUITests: XCTestCase {
 
     var app: XCUIApplication!
 
-       override func setUp() {
-           super.setUp()
-           app = XCUIApplication()
-           app.launch()
-       }
+    override func setUp() {
+        super.setUp()
+        app = XCUIApplication()
+        app.launch()
+    }
 
     func testGameUI() {
         let upButton = app.buttons["arrow.up"]
@@ -18,19 +18,19 @@ final class SnakeGameUITests: XCTestCase {
         let leftButton = app.buttons["arrow.left"]
         let rightButton = app.buttons["arrow.right"]
 
-        XCTAssertTrue(upButton.exists, "Botão para cima não encontrado")
-        XCTAssertTrue(downButton.exists, "Botão para baixo não encontrado")
-        XCTAssertTrue(leftButton.exists, "Botão para esquerda não encontrado")
-        XCTAssertTrue(rightButton.exists, "Botão para direita não encontrado")
+        XCTAssertTrue(upButton.exists, "Up button not found")
+        XCTAssertTrue(downButton.exists, "Down button not found")
+        XCTAssertTrue(leftButton.exists, "Left button not found")
+        XCTAssertTrue(rightButton.exists, "Right button not found")
     }
     
     func testSnakeVisible() {
         let snakeHead = app.otherElements["snake.head"]
-        XCTAssertTrue(snakeHead.exists, "A cabeça da cobra não está visível")
+        XCTAssertTrue(snakeHead.exists, "Snake head is not visible")
     }
 
     func testFoodVisible() {
         let food = app.otherElements["food"]
-        XCTAssertTrue(food.exists, "A comida não está visível")
+        XCTAssertTrue(food.exists, "Food is not visible")
     }
 }
